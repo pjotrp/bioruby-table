ca3e8d9c4b0e13088eadaa7ac61441d4ad55f24f
Pjotr Prins <pjotr.public01@thebird.nl>
2012-06-10 13:43:17 +0200

    features

---
+++ features/bio-table-feature.rb
@@ -0,0 +1,16 @@
+Given /^a comma separated table$/ do |string|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I read the multi\-line string$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should correctly parse the comma\-separated headers into "(.*?)","(.*?)","(.*?)"$/ do |arg1, arg2, arg3|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should correctly parse the first line into$/ do |string|
+  pending # express the regexp above with the code you wish you had
+end
+
