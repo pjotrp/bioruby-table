9db6df4360e3644ac9bd2641418271f418bdb3b1
Pjotr Prins <pjotr.public01@thebird.nl>
2012-07-23 18:55:32 +0200

    Use enumerator for input

--- lib/bio-table/tablewriter.rb
+++ lib/bio-table/tablewriter.rb
@@ -1,6 +1,16 @@
 module BioTable
 
   module TableWriter
+
+    class Writer
+      def initialize format=:tab
+        @formatter = FormatFactory::create(format)
+      end
+
+      def write row
+        @formatter.write(row.all_fields) if row.all_valid?
+      end
+    end
   end
 
 end
