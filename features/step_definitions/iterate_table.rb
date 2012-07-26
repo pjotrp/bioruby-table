e3c29298b2c1438a9f89fd5c617a4480955afc5d
Pjotr Prins <pjotr.public01@thebird.nl>
2012-07-26 11:51:53 +0200

    Version bump

--- features/step_definitions/iterate_table.rb
+++ features/step_definitions/iterate_table.rb
@@ -5,7 +5,6 @@ end
 When /^I iterate through the table$/ do
   res = []
   BioTable::TableLoader.emit(@lines, :in_format => :csv).each { |row| res << row }
-  p res
   res[3][5].should == "0.07"
 end
 
