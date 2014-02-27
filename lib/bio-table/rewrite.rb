c176310f87c375f3c70cdceafa6fec01c7518871
Pjotr Prins <pjotr.public01@thebird.nl>
2014-02-27 15:49:30 +0100

    Check for double rewrite

--- lib/bio-table/rewrite.rb
+++ lib/bio-table/rewrite.rb
@@ -4,8 +4,8 @@ module BioTable
 
     # Rewrite fields. Both field and fields can be used, but not at the same time.
     def Rewrite::rewrite code, rowname, field
-      fields = field
-      original = field
+      fields = field.dup
+      original = field.dup
       values = LazyValues.new(field)
       return rowname,field if not code or code==""
       begin
@@ -14,6 +14,13 @@ module BioTable
         $stderr.print "Failed to evaluate ",rowname," ",field," with ",code,"\n"
         raise 
       end
+      if (fields & original != fields.uniq) and (field & original != field.uniq)
+        p [:original,original]
+        p [:fields,fields]
+        p [:field,field]
+        raise "You can not rewrite both field and fields!"
+      end
+      exit
       field = fields if fields != original
       return rowname,field
     end
