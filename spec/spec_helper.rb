fb53edf1c1026fc37018d8bb0b68708f8c28aeae
Pjotr Prins <pjotr.public01@thebird.nl>
2014-06-15 10:50:03 +0200

    RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,7 @@ require 'bio-table'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 end
